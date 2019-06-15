9bd21bb1d8c1f75d89849fcc7f675b5974472dd0
Ursula Miller <ursula.miller@gmail.com>
2019-06-15 03:54:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  puts name + (katz_deli.length + 1)
+  katz_deli
+end
+
+
+def line 
+  if katz_deli.length == 0
+    "The line is currently empty"
+  else  
+    take_a_number(katz_deli, name)
+  end
+end
