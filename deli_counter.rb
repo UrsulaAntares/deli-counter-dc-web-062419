b7c2a500465b0a7ce137e74c6d0c0b0be5d8ff65
Ursula Miller <ursula.miller@gmail.com>
2019-06-15 04:10:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,8 +13,7 @@ def line(katz_deli)
   if katz_deli.count == 0
     puts "The line is currently empty."
   else  
-    puts "The line is currently: " 
-    katz_deli.each_with_index { |name, place| puts "#{place+1}. #{name} "}
+    katz_deli.each_with_index { |name, place| combo= "#{place+1}. #{name} "}
   end
 end
 
